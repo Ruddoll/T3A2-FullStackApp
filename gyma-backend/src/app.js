7b015aed3278bae439b5d54ab0b0c1a0fc12c703
Duotduot <13892@coderacademy.edu.au>
2023-08-29 13:17:38 +1000

    Adding Routes

--- gyma-backend/src/app.js
+++ gyma-backend/src/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const classesRoutes = require('./routes/classes');
+const progressRoutes = require('./routes/progress');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Test route
+app.get('/test', (req, res) => {
+  res.json({ message: 'Connected to MongoDB Atlas!' });
+});
+
 // Routes
 const routes = require('./routes'); // Create this file later
 app.use('/api', routes);
+app.use('/api/classes', classesRoutes);
+app.use('/api/progress', progressRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
